535632e7752c0cb5e92930f76a2818678c5a96d7
Aarya Paradkar <aaru7276@gmail.com>
2023-12-15 12:52:32 +0530

    working

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -10,7 +10,7 @@ import wind_icons from '../assets/wind.png';
 import humidity_icons from '../assets/humidity.png';
 
 const WeatherApp = () => {
-  let api_key = (process.env.api_key);
+  let api_key = "5e04fcf37719f9bff16c7ac025e4c5a6";
 
   const [wicon, setWicon] = useState(cloud_icons);
 
